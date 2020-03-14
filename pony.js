0a992ea9ef445e6df04ea0725a207058ab108e2a
tatsu <tatsu@andromeda.attlocal.net>
2020-03-14 15:08:52 -0700

    Fixed broken links.

--- pony.js
+++ pony.js
@@ -1,1 +1,5 @@
-document.body.innerHTML = document.body.innerHTML.replace(/Biden/g, 'the lying dog-faced pony soldier');
+document.body.innerHTML = document.body.innerHTML.replace(/\s(B|b)iden\s/g, ' the lying dog-faced pony soldier ');
+document.body.innerHTML = document.body.innerHTML.replace(/\s(B|b)iden/g, ' the lying dog-faced pony soldier');
+document.body.innerHTML = document.body.innerHTML.replace(/(B|b)iden\s/g, 'The lying dog-faced pony soldier ');
+//document.body.innerHTML = document.body.innerHTML.replace(/(Biden\'/g, 'The lying dog-faced pony soldier\'');
+//document.body.innerHTML = document.body.innerHTML.replace(/(biden\'/g, 'the lying dog-faced pony soldier\'');
