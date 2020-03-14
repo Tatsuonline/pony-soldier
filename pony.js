12fb181f80641adf95e07ce439eb0bc354a1cdde
tatsu <tatsu@andromeda.attlocal.net>
2020-03-14 15:12:55 -0700

    Removed faulty code.

--- pony.js
+++ pony.js
@@ -1,5 +1,4 @@
 document.body.innerHTML = document.body.innerHTML.replace(/\s(B|b)iden\s/g, ' the lying dog-faced pony soldier ');
 document.body.innerHTML = document.body.innerHTML.replace(/\s(B|b)iden/g, ' the lying dog-faced pony soldier');
 document.body.innerHTML = document.body.innerHTML.replace(/(B|b)iden\s/g, 'The lying dog-faced pony soldier ');
-//document.body.innerHTML = document.body.innerHTML.replace(/(Biden\'/g, 'The lying dog-faced pony soldier\'');
-//document.body.innerHTML = document.body.innerHTML.replace(/(biden\'/g, 'the lying dog-faced pony soldier\'');
+// Still need to add a fix for "Biden's".
